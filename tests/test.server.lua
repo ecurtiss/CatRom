ed53d2bb5cef32662c7cbb0b1190f1125fb6a1ac
Ethan <ethanhcurtiss@gmail.com>
2021-03-12 19:31:02 -0500

    Randomized input rotations

--- tests/test.server.lua
+++ tests/test.server.lua
@@ -10,7 +10,7 @@ for i = 1, NUM_KNOTS do
 	local part = Instance.new("Part")
 	part.Anchored = true
 	part.Size = Vector3.new(1, 1, 1)
-	part.CFrame = CFrame.new(i * 2	, math.random() * 10, math.random() * 2)
+	part.CFrame = CFrame.new(i * 2	, math.random() * 10, math.random() * 2) * CFrame.Angles(math.random(), math.random(), math.random())
 	part.Shape = Enum.PartType.Ball
 	part.Material = Enum.Material.Neon
 	part.Parent = KnotsFolder
@@ -26,7 +26,7 @@ for i = 0, 100 do
 	local part = Instance.new("Part")
 	part.Anchored = true
 	part.Size = Vector3.new(1, 1, 1) * 0.5
-	part.CFrame = Chain:GetArcRotCFrame(alpha)
+	part.CFrame = Chain:SolveRotCFrame(alpha)
 	part.Parent = PartsFolder
 end
 PartsFolder.Parent = workspace
