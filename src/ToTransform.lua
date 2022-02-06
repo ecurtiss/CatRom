d28a0cf4be403fd34c1fe72cac756b85d2faa6cd
Ethan <ethanhcurtiss@gmail.com>
2022-02-06 01:05:33 -0500

    Use faster CFrame to quaternion

--- src/ToTransform.lua
+++ src/ToTransform.lua
@@ -4,50 +4,10 @@
 ]]
 
 local function CFrameToQuaternion(cframe)
-	local _, _, _, m00, m01, m02, m10, m11, m12, m20, m21, m22 = cframe:GetComponents()
-	local trace = m00 + m11 + m22
-	if trace > 0 then
-		local s = math.sqrt(1 + trace)
-		local reciprocal = 0.5 / s
-		return {
-			s / 2,
-			(m21 - m12) * reciprocal,
-			(m02 - m20) * reciprocal,
-			(m10 - m01) * reciprocal
-		}
-	else
-		local big = math.max(m00, m11, m22)
-		if big == m00 then
-			local s = math.sqrt(1 + m00 - m11 - m22)
-			local reciprocal = 0.5 / s
-			return {
-				(m21 - m12) * reciprocal,
-				s / 2,
-				(m10 + m01) * reciprocal,
-				(m02 + m20) * reciprocal
-			}
-		elseif big == m11 then
-			local s = math.sqrt(1 - m00 + m11 - m22)
-			local reciprocal = 0.5 / s
-			return {
-				(m02 - m20) * reciprocal,
-				(m10 + m01) * reciprocal,
-				s / 2,
-				(m21 + m12) * reciprocal
-			}
-		elseif big == m22 then
-			local s = math.sqrt(1 - m00 - m11 + m22 )
-			local reciprocal = 0.5 / s
-			return {
-				(m10 - m01) * reciprocal,
-				(m02 + m20) * reciprocal,
-				(m21 + m12) * reciprocal,
-				s / 2
-			}
-		else
-			return table.create(4, nil)
-		end
-	end
+	local axis, angle = cframe:ToAxisAngle()
+	angle /= 2
+	axis = math.sin(angle) * axis
+	return {math.cos(angle), axis.X, axis.Y, axis.Z}
 end
 
 local function ToTransform(point, pointType)
