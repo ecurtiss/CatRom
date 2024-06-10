9c79dacfde6dffb0b79be806cbbffb6ca6fc8509
Ella <ethanhcurtiss@gmail.com>
2024-06-10 15:23:11 -0400

    Include MIT license in SQUAD

--- src/Squad.lua
+++ src/Squad.lua
@@ -1,5 +1,7 @@
 -- Spherical quadrangle interpolation (SQUAD)
--- By Fractality (https://github.com/Fraktality/squad)
+-- Copyright (c) 2019 Parker Stebbins
+-- Distributed under the MIT license; see full notice at the end of this file.
+-- github.com/Fraktality/squad
 
 local Types = require(script.Parent.Types)
 
@@ -98,3 +100,27 @@ local function Squad(
 end
 
 return Squad
+
+--[[
+	MIT License
+
+	Copyright (c) 2019 Parker Stebbins
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+]]
